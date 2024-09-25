bf339743a64990d7e0b1e784f4048c72541ea0ae
thesmartwon <thesmartwon@pm.me>
2024-09-25 14:02:12 -0400

    add number prefix to file name

--- src/main.js
+++ src/main.js
@@ -7,6 +7,75 @@ import { parseTagnt } from './tat/tagnt.js';
 
 const outdir = 'dist';
 
+const order = [
+"gen",
+"exo",
+"lev",
+"num",
+"deu",
+"jos",
+"jdg",
+"rut",
+"1sa",
+"2sa",
+"1ki",
+"2ki",
+"1ch",
+"2ch",
+"ezr",
+"neh",
+"est",
+"job",
+"psa",
+"pro",
+"ecc",
+"sng",
+"isa",
+"jer",
+"lam",
+"ezk",
+"dan",
+"hos",
+"jol",
+"amo",
+"oba",
+"jon",
+"mic",
+"nam",
+"hab",
+"zep",
+"hag",
+"zec",
+"mal",
+"mat",
+"mrk",
+"luk",
+"jhn",
+"act",
+"rom",
+"1co",
+"2co",
+"gal",
+"eph",
+"php",
+"col",
+"1th",
+"2th",
+"1ti",
+"2ti",
+"tit",
+"phm",
+"heb",
+"jas",
+"1pe",
+"2pe",
+"1jn",
+"2jn",
+"3jn",
+"jud",
+"rev",
+];
+
 /** @param {string} fname */
 async function parseFile(fname) {
 	const file = await open(fname);
@@ -26,7 +95,11 @@ async function parseFile(fname) {
 	}
 	else throw Error(`${fname} has unknown file type`);
 
-	const outpath = join(outdir, id, `${basename(fname).split(' ')[1]}.csv`);
+	const bookRange = basename(fname).split(' ')[1];
+	const firstBook = bookRange.split('-')[0].toLowerCase();
+	const n = order.indexOf(firstBook);
+	if (n == -1) throw Error(`unknown book ${firstBook}`);
+	const outpath = join(outdir, id, `${(n + 1).toString().padStart(2, '0')}-${bookRange}.csv`);
 	console.log(fname, '->', outpath);
 
 	const out = fastcsv.format({ headers: true, delimiter: '|' });
